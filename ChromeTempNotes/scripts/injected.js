ebef7ae4f8e8132e20ec9db2ddefd056f24b209d
Nathan Tucker <abezhat@verizon.net>
2016-10-27 13:07:04 -0700

    Fixing up bug with close buttons being deleted

--- ChromeTempNotes/scripts/injected.js
+++ ChromeTempNotes/scripts/injected.js
@@ -10,40 +10,48 @@ $( 'body' ).contextmenu( function( e ) {
     last_click_pos.y = e.pageY;
 });
 
-//TODO: fix up bug with deleting X icon
 chrome.extension.onMessage.addListener( function ( message, sender, callback ) {
     if ( message.function == "context_menu_clicked" ) {
-
         $( '<div/>', {
-            'id' : 'note-' + note_id,
-            'class' : 'note-div note-div-dark',
-            'contenteditable' : 'true'
+            'id' : 'note-parent-' + note_id,
+            'class' : 'note-div-parent note-div-dark',
+            'contenteditable' : 'false'
         }).css({
             left : last_click_pos.x - 100,
             top : last_click_pos.y - 50
         }).appendTo( 'body' );
 
+        $( '<div/>', {
+            'id' : 'note-' + note_id,
+            'class' : 'note-div',
+            'contenteditable' : 'true'
+        }).appendTo( '#note-parent-' + note_id );
+
         $( '<span/>', {
             'id' : 'toggle-' + note_id,
             'class' : 'toggle-color',
             'contenteditable' : 'false'
-        }).appendTo( '#note-' + note_id );
+        }).appendTo( '#note-parent-' + note_id );
 
         $( '<span/>', {
             'id' : 'close-' + note_id,
             'class' : 'close',
             'contenteditable' : 'false'
-        }).appendTo( '#note-' + note_id );
+        }).appendTo( '#note-parent-' + note_id );
 
-        $( '#note-' + note_id ).draggable( );
+        $( '#note-parent-' + note_id ).draggable( );
         $( '#note-' + note_id ).focus( );
 
+        $( '#note-parent-' + note_id ).on( 'click', function() {
+            $(this).children( '.note-div' ).focus( );
+        });
+
         $( '#close-' + note_id ).on( 'click', function( ) {
-            $( this ).parents( '.note-div' ).remove( );
+            $( this ).parents( '.note-div-parent' ).remove( );
         });
 
         $( '#toggle-' + note_id ).on( 'click', function() {
-            $( this ).parents( '.note-div' ).toggleClass( 'note-div-dark note-div-light')
+            $( this ).parents( '.note-div-parent' ).toggleClass( 'note-div-dark note-div-light')
         });
 
         note_id++;
