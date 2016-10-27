7c42551f2539850daf2bf202edd5593c31176b1b
Nathan Tucker <abezhat@verizon.net>
2016-10-27 11:13:36 -0700

    Adding color switcher

--- ChromeTempNotes/scripts/injected.js
+++ ChromeTempNotes/scripts/injected.js
@@ -11,18 +11,23 @@ $( 'body' ).contextmenu( function( e ) {
 });
 
 //TODO: fix up bug with deleting X icon
-//TODO: add color switcher
 chrome.extension.onMessage.addListener( function ( message, sender, callback ) {
     if ( message.function == "context_menu_clicked" ) {
 
         $( '<div/>', {
             'id' : 'note-' + note_id,
-            'class' : 'note-div',
+            'class' : 'note-div note-div-dark',
             'contenteditable' : 'true'
         }).css({
             left : last_click_pos.x - 100,
             top : last_click_pos.y - 50
         }).appendTo( 'body' );
+
+        $( '<span/>', {
+            'id' : 'toggle-' + note_id,
+            'class' : 'toggle-color',
+            'contenteditable' : 'false'
+        }).appendTo( '#note-' + note_id );
 
         $( '<span/>', {
             'id' : 'close-' + note_id,
@@ -35,6 +40,10 @@ chrome.extension.onMessage.addListener( function ( message, sender, callback ) {
 
         $( '#close-' + note_id ).on( 'click', function( ) {
             $( this ).parents( '.note-div' ).remove( );
+        });
+
+        $( '#toggle-' + note_id ).on( 'click', function() {
+            $( this ).parents( '.note-div' ).toggleClass( 'note-div-dark note-div-light')
         });
 
         note_id++;
